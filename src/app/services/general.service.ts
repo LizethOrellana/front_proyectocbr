aafafa00337b2daf5aa8cb5f28fa9a32581ef63f
LizethOrellana <97999828+LizethOrellana@users.noreply.github.com>
2025-04-20 20:36:25 -0500

    url

--- src/app/services/general.service.ts
+++ src/app/services/general.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GeneralService {
-  public baseUrl = 'https://18.218.153.234/';
+  public baseUrl = 'https://18.218.153.234';
 
   constructor() { }
 }
