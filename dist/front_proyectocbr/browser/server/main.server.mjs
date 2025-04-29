0e30e0dd48d0c1a455227fb321e37ef0225acee9
LizethOrellana <97999828+LizethOrellana@users.noreply.github.com>
2025-04-29 10:43:16 -0500

    Cambio de derechos de autor footer

--- dist/front_proyectocbr/browser/server/main.server.mjs
+++ dist/front_proyectocbr/browser/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Z2ZV2TM5.mjs";import"./chunk-BYCKACKA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3TSWDWV3.mjs";import"./chunk-BYCKACKA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
